010d4f31be7543032b940d19d957bdf63c3c4578
Jhoe Ohaze <ohaze.jhoe@gmail.com>
2020-01-31 16:15:06 -0300

    added send function

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:chat_online/ui/text_composer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget
@@ -10,13 +13,24 @@ class HomePage extends StatefulWidget
 
 class _HomePageState extends State<HomePage>
 {
-  void _sendMessage(String text)
+  void _sendMessage({String text, File imgFile}) async
   {
+    Map<String, dynamic> data = {};
+
     Firestore.instance.collection('messages').add(
     {
       'text': text,
-      'date': DateTime.now().toUtc(),
     });
+    if(imgFile != null)
+    {
+      StorageUploadTask task = FirebaseStorage.instance.ref().child(
+          DateTime.now().millisecondsSinceEpoch.toString()).putFile(imgFile);
+      StorageTaskSnapshot taskSnapshot = await task.onComplete;
+      String url = await taskSnapshot.ref.getDownloadURL();
+      data['imgUrl'] = url;
+    }
+
+    if(text != null) data['text'] = text;
   }
   
   @override
@@ -30,12 +44,42 @@ class _HomePageState extends State<HomePage>
         elevation: 0,
         backgroundColor: Colors.blue,
       ),
-      body: TextComposer
+      body: Column
       (
-        (text)
-        {
-          print(text);
-        }
+        children: <Widget>
+        [
+          Expanded
+          (
+            child: StreamBuilder
+            (
+              stream: Firestore.instance.collection('messages').snapshots(),
+              builder: (context, snapshot)
+              {
+                switch(snapshot.connectionState)
+                {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return Center
+                    (
+                      child: CircularProgressIndicator(),
+                    );
+                  default:
+                    List<DocumentSnapshot> documents = snapshot.data.documents;
+                    return ListView.builder
+                    (
+                      itemCount: documents.length,
+                      reverse: true,
+                      itemBuilder: (context, index)
+                        {
+                          
+                        }
+                    );
+                }
+              },
+            ),
+          ),
+          TextComposer(_sendMessage),
+        ]
       ),
     );
   }
